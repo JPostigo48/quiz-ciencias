61ae90cdab25cb7f31bc4c81aea750662b627b73
Juan Carlos Postigo <jpostigo@unsa.edu.pe>
2021-08-11 21:46:11 -0500

    Updates

---
+++ precache-manifest.3351df197de7ba83ba18671724a95418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178b845e723b3c41adac0818391dcaed",
+    "url": "/quiz-ciencias/index.html"
+  },
+  {
+    "revision": "af08414f2eb233800532",
+    "url": "/quiz-ciencias/static/css/main.107cffdf.chunk.css"
+  },
+  {
+    "revision": "34c3fafb4817ce8be14a",
+    "url": "/quiz-ciencias/static/js/2.71dc2452.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/quiz-ciencias/static/js/2.71dc2452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af08414f2eb233800532",
+    "url": "/quiz-ciencias/static/js/main.e1c6659a.chunk.js"
+  },
+  {
+    "revision": "5bc416ddb2b37de5ca28",
+    "url": "/quiz-ciencias/static/js/runtime-main.110e348c.js"
+  }
+]);
